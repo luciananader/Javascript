f32291e4a14ee9f9afbacdaa14c00afffd28bb2c
Luciana Leal Nader <118460220+luciananader@users.noreply.github.com>
2023-03-21 22:04:09 -0300

    alteração

--- AULA 12 EXER/script.js
+++ AULA 12 EXER/script.js
@@ -1,7 +1,21 @@
-function carregar(){
-    var txt = document.getElementById('txt')
-    var img = document.getElementById('imagem')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    txt.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora>=0 && hora<12){
+        //Bom dia!!
+        img.src = 'fotomanha.png'
+        document.body.style.background ='#dcd4cb'
+    } else if (hora>=12 && hora<18){
+        //Boa tarde!!
+        img.src = 'fototarde.png'
+        document.body.style.background ='#aa8691'
+    } else{
+        //Boa noite!!
+        img.src = 'fotonoite.png'
+        document.body.style.background ='#9d4152'
+       
+    }
 }
