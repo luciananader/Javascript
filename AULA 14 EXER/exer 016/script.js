3fa568127cce487bdc0f0c83a5c88cfd745576bf
Luciana Leal Nader <118460220+luciananader@users.noreply.github.com>
2023-03-24 10:36:28 -0300

    alteração

---
+++ AULA 14 EXER/exer 016/script.js
@@ -0,0 +1,19 @@
+function carregar() {
+    var ini = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var pass = document.getElementById('passo')
+    var res = document.getElementById('res')
+
+    var res = 
+    if(var ini == 0 || var fim == 0){
+        res.innerHTML=('Impossivel contar!')
+    }
+    if(var pass == 0){
+        window.alert('Passo inválido! Considere a partir do passo 1!')
+    }
+    if(var fim>=var ini){
+        window.alert('Erro!! O início deve ser menor que o fim!')
+    }
+
+    var res = 
+ }
