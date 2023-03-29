d94596a0edb589dd824b726c8446c7b4e7637e09
Luciana Leal Nader <118460220+luciananader@users.noreply.github.com>
2023-03-29 18:39:30 -0300

    alteração

--- ULTIMO PROJETO/script.js
+++ ULTIMO PROJETO/script.js
@@ -1,6 +1,7 @@
 function adicionar (){
 var num = document.getElementById('numero')
 var add = document.getElementById('valor')
+var res = document.getElementById('result')
 var n = Number (num.value)
     if (num.value.length == 0){
         window.alert('ERRO! Escolha um número!')
