693c2c8e680921539aea51eb08b9535c57c3b10f
Luciana Leal Nader <118460220+luciananader@users.noreply.github.com>
2023-03-29 17:56:17 -0300

    alteracao

---
+++ ULTIMO PROJETO/script.js
@@ -0,0 +1,15 @@
+function adicionar (){
+var num = document.getElementById('numero')
+var add = document.getElementById('valor')
+var n = Number (num.value)
+    if (num.value.length == 0){
+        window.alert('ERRO! Escolha um número!')
+    } else if (num.value <= 0 || num.value >100){
+        window.alert('ERRO! Necessário colocar um núemro entre 1 e 100')
+    } else {
+        var item = document.createElement('option')
+        item.text = `Valor ${n} adicionado.`
+        add.appendChild (item)     
+    }
+
+}        
